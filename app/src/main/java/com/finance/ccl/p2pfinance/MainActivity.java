165e0203a2f9a6e20ae3a40e231836c1e07b22e3
chencl <chencl@spreadst.com>
2018-04-09 16:30:16 +0800

    初始化项目，提交资源文件

--- app/src/main/java/com/finance/ccl/p2pfinance/MainActivity.java
+++ app/src/main/java/com/finance/ccl/p2pfinance/MainActivity.java
@@ -1,13 +1,48 @@
 package com.finance.ccl.p2pfinance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends Activity {
+
+    @InjectView(R.id.content)
+    FrameLayout content;
+    @InjectView(R.id.iv_home)
+    ImageView ivHome;
+    @InjectView(R.id.tv_home)
+    TextView tvHome;
+    @InjectView(R.id.ll_home)
+    LinearLayout llHome;
+    @InjectView(R.id.iv_touzi)
+    ImageView ivTouzi;
+    @InjectView(R.id.tv_touzi)
+    TextView tvTouzi;
+    @InjectView(R.id.ll_touzi)
+    LinearLayout llTouzi;
+    @InjectView(R.id.iv_me)
+    ImageView ivMe;
+    @InjectView(R.id.tv_me)
+    TextView tvMe;
+    @InjectView(R.id.ll_me)
+    LinearLayout llMe;
+    @InjectView(R.id.iv_more)
+    ImageView ivMore;
+    @InjectView(R.id.tv_more)
+    TextView tvMore;
+    @InjectView(R.id.ll_more)
+    LinearLayout llMore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 }
---
+++ app/src/main/java/com/finance/ccl/p2pfinance/fragment/TouziFragment.java
@@ -0,0 +1,28 @@
+package com.finance.ccl.p2pfinance.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.finance.ccl.p2pfinance.R;
+
+/**
+ * Created by ccl on 18-4-9.
+ */
+
+public class TouziFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(container.getContext(), R.layout.fragment_touzi,null);
+        return view;
+    }
+}
