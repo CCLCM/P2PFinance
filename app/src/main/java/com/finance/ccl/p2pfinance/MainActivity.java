edc60c38b8596e89a22525954d398773ff845924
chencl <chencl@spreadst.com>
2018-04-09 18:03:15 +0800

    提交部分文件实现fragment

--- app/src/main/java/com/finance/ccl/p2pfinance/MainActivity.java
+++ app/src/main/java/com/finance/ccl/p2pfinance/MainActivity.java
@@ -2,15 +2,25 @@ package com.finance.ccl.p2pfinance;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.finance.ccl.p2pfinance.fragment.HomeFragment;
+import com.finance.ccl.p2pfinance.fragment.MeFragment;
+import com.finance.ccl.p2pfinance.fragment.MoreFragment;
+import com.finance.ccl.p2pfinance.fragment.TouziFragment;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @InjectView(R.id.content)
     FrameLayout content;
@@ -38,6 +48,11 @@ public class MainActivity extends Activity {
     TextView tvMore;
     @InjectView(R.id.ll_more)
     LinearLayout llMore;
+    private HomeFragment homeFragment;
+    private TouziFragment touziFragment;
+    private MeFragment meFragment;
+    private MoreFragment moreFragment;
+    private FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,4 +60,86 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
     }
+
+    @OnClick({R.id.ll_home,R.id.ll_touzi,R.id.ll_me,R.id.ll_more})
+    public void  changeTab(View view){
+        switch (view.getId()){
+            case R.id.ll_home:
+                setSelect(0);
+                break;
+            case R.id.ll_touzi:
+                setSelect(1);
+                break;
+            case R.id.ll_me:
+                setSelect(2);
+                break;
+            case R.id.ll_more:
+                setSelect(3);
+                break;
+            default:
+        }
+
+    }
+
+    private void setSelect(int i) {
+        FragmentManager fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        hideFragment();
+        switch (i) {
+            case 0:
+                //首页
+                if (homeFragment == null) {
+                    homeFragment = new HomeFragment();
+                    ft.add(R.id.content, homeFragment);
+                }
+                ft.show(homeFragment);
+                break;
+            case 1:
+                //投资
+                if (touziFragment == null) {
+                    touziFragment = new TouziFragment();
+                    ft.add(R.id.content, touziFragment);
+                }
+                ft.show(touziFragment);
+                break;
+            case 2:
+                //我的
+                if (meFragment == null) {
+                    meFragment = new MeFragment();
+                    ft.add(R.id.content, meFragment);
+                }
+                ft.show(meFragment);
+                break;
+            case 3:
+                //更多
+                if (moreFragment == null) {
+                    moreFragment = new MoreFragment();
+                    ft.add(R.id.content, moreFragment);
+                }
+                ft.show(moreFragment);
+                break;
+            default:
+        }
+        ft.commit();
+    }
+
+    private void hideFragment() {
+        if (homeFragment != null) {
+           ft.hide(homeFragment);
+        }
+
+        if (touziFragment != null) {
+            ft.hide(touziFragment);
+        }
+
+        if (meFragment != null) {
+            ft.hide(meFragment);
+        }
+
+        if (moreFragment != null) {
+            ft.hide(moreFragment);
+        }
+    }
+
+
 }
