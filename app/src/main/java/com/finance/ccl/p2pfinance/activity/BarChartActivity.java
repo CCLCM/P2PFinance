9e7641152ab01a5fb599fe19f20b50c9e8763992
chencl <ccl_cm@163.com>
2018-11-05 19:36:42 +0800

    饼状图和柱状图

--- app/src/main/java/com/finance/ccl/p2pfinance/activity/BarChartActivity.java
+++ app/src/main/java/com/finance/ccl/p2pfinance/activity/BarChartActivity.java
@@ -1,5 +1,6 @@
 package com.finance.ccl.p2pfinance.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,6 +9,14 @@ import android.widget.TextView;
 import com.finance.ccl.p2pfinance.R;
 import com.finance.ccl.p2pfinance.common.BaseActivity;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,19 +31,74 @@ public class BarChartActivity extends BaseActivity {
     ImageView titleRight;
     @BindView(R.id.chart)
     BarChart chart;
-
+    private Typeface mTf;
     @Override
     protected void initData() {
+        mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+// apply styling
+        chart.getDescription().setEnabled(false);
+        chart.setDrawGridBackground(false);
+        chart.setDrawBarShadow(false);
 
+        XAxis xAxis =  chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTypeface(mTf);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+
+        YAxis leftAxis =  chart.getAxisLeft();
+        leftAxis.setTypeface(mTf);
+        leftAxis.setLabelCount(5, false);
+        leftAxis.setSpaceTop(20f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis =  chart.getAxisRight();
+        rightAxis.setTypeface(mTf);
+        rightAxis.setLabelCount(5, false);
+        rightAxis.setSpaceTop(20f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        generateDataBar().setValueTypeface(mTf);
+
+        // set data
+        chart.setData(generateDataBar());
+        chart.setFitBars(true);
+
+        // do not forget to refresh the chart
+//        holder.chart.invalidate();
+        chart.animateY(700);
+
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Bar data
+     */
+    private BarData generateDataBar() {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            entries.add(new BarEntry(i, (int) (Math.random() * 70) + 30));
+        }
+
+        BarDataSet d = new BarDataSet(entries, "New DataSet ");
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        d.setHighLightAlpha(255);
+
+        BarData cd = new BarData(d);
+        cd.setBarWidth(0.9f);
+        return cd;
     }
 
     @Override
     protected void initTitle() {
-        titleTv.setText("饼状图");
+        titleTv.setText("柱状图");
         titleLeft.setVisibility(View.VISIBLE);
         titleRight.setVisibility(View.INVISIBLE);
     }
-    @OnClick(R.id.title_right)
+    @OnClick(R.id.title_left)
     public void  back(View view){
         closeCurrent();
     }
--- app/src/main/java/com/finance/ccl/p2pfinance/activity/PieChartActivity.java
+++ app/src/main/java/com/finance/ccl/p2pfinance/activity/PieChartActivity.java
@@ -1,6 +1,11 @@
 package com.finance.ccl.p2pfinance.activity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,12 +13,21 @@ import android.widget.TextView;
 import com.finance.ccl.p2pfinance.R;
 import com.finance.ccl.p2pfinance.common.BaseActivity;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class PieChartActivity extends BaseActivity {
+
     @BindView(R.id.title_left)
     ImageView titleLeft;
     @BindView(R.id.title_tv)
@@ -22,20 +36,85 @@ public class PieChartActivity extends BaseActivity {
     ImageView titleRight;
     @BindView(R.id.chart)
     PieChart chart;
-
+    private Typeface mTf;
+    private SpannableString mCenterText;
     @Override
     protected void initData() {
+        mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
 
+        // apply styling
+        chart.getDescription().setEnabled(false);chart.setHoleRadius(52f);
+        chart.setTransparentCircleRadius(57f);
+        chart.setCenterText(mCenterText);
+        chart.setCenterTextTypeface(mTf);
+        chart.setCenterTextSize(9f);
+        chart.setUsePercentValues(true);
+        chart.setExtraOffsets(5, 10, 50, 10);
+
+        generateDataPie().setValueFormatter(new PercentFormatter());
+        generateDataPie().setValueTypeface(mTf);
+        generateDataPie().setValueTextSize(11f);
+        generateDataPie().setValueTextColor(Color.WHITE);
+        // set data
+         chart.setData((PieData) generateDataPie());
+
+        Legend l =  chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        // do not forget to refresh the chart
+        // holder.chart.invalidate();
+         chart.animateY(900);
+    }
+
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Pie data
+     */
+    private PieData generateDataPie() {
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * 70) + 30), "Quarter " + (i+1)));
+        }
+
+        PieDataSet d = new PieDataSet(entries, "");
+
+        // space between slices
+        d.setSliceSpace(2f);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        return new PieData(d);
+    }
+
+
+
+    private SpannableString generateCenterText() {
+        SpannableString s = new SpannableString("by\nChencl");
+        s.setSpan(new RelativeSizeSpan(1.6f), 0, 14, 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.VORDIPLOM_COLORS[0]), 0, 14, 0);
+        s.setSpan(new RelativeSizeSpan(.9f), 14, 25, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, 25, 0);
+        s.setSpan(new RelativeSizeSpan(1.4f), 25, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), 25, s.length(), 0);
+        return s;
     }
 
     @Override
     protected void initTitle() {
-        titleTv.setText("柱状图");
+        titleTv.setText("饼状图");
         titleLeft.setVisibility(View.VISIBLE);
         titleRight.setVisibility(View.INVISIBLE);
     }
 
-    @OnClick(R.id.title_right)
+    @OnClick(R.id.title_left)
     public void  back(View view){
         closeCurrent();
     }
